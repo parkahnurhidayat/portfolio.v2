709d8130b86aec445c792bd1aab05dff49f4c55b
parkah <parkah098@gmail.com>
2024-03-07 22:36:35 +0700

    update project

--- src/app/view/Dashboard/Project/page.tsx
+++ src/app/view/Dashboard/Project/page.tsx
@@ -9,6 +9,42 @@ const ProjectPageView = () => {
         <p className="text-slate-500 font-medium uppercase ">featuring all of my projects about technology</p>
       </div>
       <div className="flex flex-wrap gap-6 border-t py-10">
+
+        <div className="border cursor-pointer rounded-xl shadow-md lg:max-w-sm  hover:scale-105 transition-all duration-700 overflow-hidden group:">
+          <Link href={"https://weather-six-ashy.vercel.app/"} target="_blank">
+
+            <Image src="/project/cuaca.png" className="w-full h-48 object-cover" alt="project1" width={1000} height={1000} />
+            <div className="p-5">
+              <h2 className="text-md text-slate-800 uppercase pt-5 pb-2">Weather App</h2>
+              <p className="text-slate-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, atque.</p>
+              <div className="flex gap-x-4 pt-5">
+                <Image src="/asset/Tailwind CSS.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+                <Image src="/asset/Javascript.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+
+                <Image src="/asset/React.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+              </div>
+            </div>
+
+          </Link>
+        </div>
+
+
+        <div className="border cursor-pointer rounded-xl shadow-md lg:max-w-sm  hover:scale-105 transition-all duration-700 overflow-hidden">
+          <Link href={"https://quotes-random-six.vercel.app/"} target="_blank">
+
+            <Image src="/project/quotes.png" className="w-full h-48 object-cover" alt="project1" width={1000} height={1000} />
+            <div className="p-5">
+              <h2 className="text-md text-slate-800 uppercase pt-5 pb-2">Quotes Random</h2>
+              <p className="text-slate-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, atque.</p>
+              <div className="flex gap-x-4 pt-5">
+                <Image src="/asset/Tailwind CSS.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+                <Image src="/asset/Javascript.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+                <Image src="/asset/Vite.js.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+                <Image src="/asset/React.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
+              </div>
+            </div>
+          </Link>
+        </div>
         <div className="border cursor-pointer rounded-xl shadow-md lg:max-w-sm  hover:scale-105 transition-all duration-700 overflow-hidden group:">
           <Link href={"https://parkah-portfolio.vercel.app/"} target="_blank">
 
@@ -24,23 +60,6 @@ const ProjectPageView = () => {
               </div>
             </div>
 
-
-          </Link>
-        </div>
-        <div className="border cursor-pointer rounded-xl shadow-md lg:max-w-sm  hover:scale-105 transition-all duration-700 overflow-hidden">
-          <Link href={"https://quotes-random-six.vercel.app/"} target="_blank">
-
-            <Image src="/project/quotes.png" className="w-full h-48 object-cover" alt="project1" width={1000} height={1000} />
-            <div className="p-5">
-              <h2 className="text-md text-slate-800 uppercase pt-5 pb-2">Quotes Random</h2>
-              <p className="text-slate-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, atque.</p>
-              <div className="flex gap-x-4 pt-5">
-                <Image src="/asset/Tailwind CSS.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
-                <Image src="/asset/Javascript.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
-                <Image src="/asset/Vite.js.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
-                <Image src="/asset/React.png" width={25} height={25} alt="tech" className="hover:scale-125 transition-all duration-500 cursor-pointer" />
-              </div>
-            </div>
           </Link>
         </div>
         <div className="border cursor-pointer rounded-xl shadow-md lg:max-w-sm  hover:scale-105 transition-all duration-700 overflow-hidden">
